fbe6452c5dc06c8283f1bea55aae570fffde5d03
Chris Vouga <crvouga@gmail.com>
2025-02-21 21:02:51 -0700

    Add Tailwind CSS configuration and update project structure

---
+++ src/@/use-query-param.ts
@@ -0,0 +1,12 @@
+import { Codec } from "./codec";
+
+export const useQueryParam = <T>(config: {
+  param: string;
+  defaultValue: T;
+  codec: Codec<T>;
+}) => {
+  const { param, defaultValue } = config;
+  const searchParams = useSearchParams();
+  const value = searchParams.get(param) || defaultValue;
+  return value;
+};
