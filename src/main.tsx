b4d7cd63c4d3e28547627ffe4fcfa6ef55e9f2fd
Chris Vouga <crvouga@gmail.com>
2025-03-15 11:26:41 -0700

    Remove index.css and update imports in main.tsx to use app.tsx and main.css

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from './App.tsx';
-import './index.css';
+import { App } from './app_.tsx';
+import './main.css';
 
 const root = document.getElementById('root');
 
