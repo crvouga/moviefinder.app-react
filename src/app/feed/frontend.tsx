a71ad9f22fc781d49bc9bcc354d2694ce94536bd
Chris Vouga <49178412+crvouga@users.noreply.github.com>
2025-05-12 14:51:51 -0700

    Refactor ViewMediaDbQueryOutput component to streamline media loading logic; replace media query handling with a dedicated SlidesFragment for improved clarity and maintainability; update loading state representation with a new ImgLoading component.

--- src/app/feed/frontend.tsx
+++ src/app/feed/frontend.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import { ImageSet } from '~/@/image-set'
 import { useSubscription } from '~/@/pub-sub'
-import { NotAsked, Remote } from '~/@/result'
+import { Loading } from '~/@/result'
 import { Img } from '~/@/ui/img'
 import { PreloadImg } from '~/@/ui/preload-img'
 import { Swiper } from '~/@/ui/swiper'
@@ -9,7 +9,7 @@ import { useCurrentScreen } from '../@/screen/use-current-screen'
 import { useAppBottomButtons } from '../@/ui/app-bottom-buttons'
 import { ScreenLayout } from '../@/ui/screen-layout'
 import { useCtx } from '../frontend/ctx'
-import { MediaDbQueryOutput } from '../media/media-db/interface/query-output'
+import { Media } from '../media/media'
 import { Feed } from './feed'
 import { FeedDbQueryOutput } from './feed-db/interface/query-output'
 import { FeedId } from './feed-id'
@@ -37,76 +37,96 @@ export const FeedScreen = () => {
     }
   }, [feedQuery])
 
-  const mediaQuery = useSubscription(
-    () =>
-      ctx.mediaDb.liveQuery({
-        limit: 20,
-        offset: 0,
-        orderBy: [{ column: 'popularity', direction: 'desc' }],
-      }),
-    [ctx]
-  )
-
   const appBottomButtons = useAppBottomButtons()
   return (
     <ScreenLayout topBar={{ title: 'Feed' }} actions={appBottomButtons}>
-      <ViewMediaDbQueryOutput media={mediaQuery ?? NotAsked} feed={feed} />
+      <ViewMediaDbQueryOutput feed={feed} />
     </ScreenLayout>
   )
 }
 
-const ViewMediaDbQueryOutput = (props: {
-  media: Remote | MediaDbQueryOutput
-  feed: Feed | null
-}) => {
-  const currentScreen = useCurrentScreen()
+const ViewMediaDbQueryOutput = (props: { feed: Feed | null }) => {
   const ctx = useCtx()
-  switch (props.media.t) {
-    case 'not-asked':
+
+  const { feed } = props
+
+  if (!feed) return <ImgLoading />
+
+  return (
+    <Swiper.Container
+      initialSlide={feed.activeIndex}
+      slidesPerView={1}
+      className="h-full w-full"
+      direction="vertical"
+      onSlideChange={({ activeIndex }) => {
+        ctx.feedDb.upsert([{ ...feed, activeIndex }])
+      }}
+    >
+      <SlidesFragment limit={20} offset={0} />
+    </Swiper.Container>
+  )
+}
+
+const SlidesFragment = (props: { limit: number; offset: number }) => {
+  const ctx = useCtx()
+
+  const mediaQuery = useSubscription(
+    () =>
+      ctx.mediaDb.liveQuery({
+        limit: props.limit,
+        offset: props.offset,
+        orderBy: [{ column: 'popularity', direction: 'desc' }],
+      }),
+    [ctx]
+  )
+  const media = mediaQuery ?? Loading
+  switch (media.t) {
+    case 'error':
     case 'loading': {
-      return <Loading />
-    }
-    case 'error': {
-      return <div>Error</div>
+      return (
+        <Swiper.Slide>
+          <ImgLoading />
+        </Swiper.Slide>
+      )
     }
     case 'ok': {
-      if (props.media.value.media.items.length === 0) return <Loading />
-
-      const { feed } = props
-
-      if (!feed) return <Loading />
+      if (media.value.media.items.length === 0)
+        return (
+          <Swiper.Slide>
+            <ImgLoading />
+          </Swiper.Slide>
+        )
 
       return (
-        <Swiper.Container
-          initialSlide={feed.activeIndex}
-          slidesPerView={1}
-          className="h-full w-full"
-          direction="vertical"
-          onSlideChange={({ activeIndex }) => {
-            ctx.feedDb.upsert([{ ...feed, activeIndex }])
-          }}
-        >
-          {props.media.value.media.items.map((item) => (
+        <>
+          {media.value.media.items.map((item) => (
             <Swiper.Slide key={item.id}>
-              <button
-                className="h-full w-full cursor-pointer"
-                onClick={() => currentScreen.push({ type: 'media-details', mediaId: item.id })}
-              >
-                <Img
-                  className="h-full w-full object-cover"
-                  src={ImageSet.toHighestRes(item.poster)}
-                  alt={item.title}
-                />
-                <PreloadImg image={ImageSet.toHighestRes(item.backdrop)} />
-              </button>
+              <SlideContent item={item} />
             </Swiper.Slide>
           ))}
-        </Swiper.Container>
+        </>
       )
     }
   }
 }
 
-const Loading = () => {
+const SlideContent = (props: { item: Media }) => {
+  const currentScreen = useCurrentScreen()
+  return (
+    <button
+      className="h-full w-full cursor-pointer"
+      onClick={() => currentScreen.push({ type: 'media-details', mediaId: props.item.id })}
+    >
+      <Img
+        className="h-full w-full object-cover"
+        src={ImageSet.toHighestRes(props.item.poster)}
+        alt={props.item.title}
+      />
+      <PreloadImg image={ImageSet.toHighestRes(props.item.backdrop)} />
+    </button>
+  )
+}
+
+const ImgLoading = () => {
   return <Img className="h-full w-full object-cover" alt="Loading..." />
 }
