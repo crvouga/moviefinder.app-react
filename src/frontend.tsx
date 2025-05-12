d59dc8fefeabd789d16017c9d37f9c6e9e6a66d3
Chris Vouga <crvouga@gmail.com>
2025-05-11 17:28:26 -0700

    Refactor frontend initialization to use an async main function; improve readability by organizing context setup and rendering logic within the main function.

--- src/frontend.tsx
+++ src/frontend.tsx
@@ -3,21 +3,25 @@ import ReactDOM from 'react-dom/client'
 import { App } from './app/frontend.tsx'
 import { Ctx } from '~/app/ctx/frontend.tsx'
 
-const root = document.getElementById('root')
+const main = async () => {
+  const root = document.getElementById('root')
 
-if (!root) throw new Error('Root element not found')
+  if (!root) throw new Error('Root element not found')
 
-const reactRoot = ReactDOM.createRoot(root)
+  const reactRoot = ReactDOM.createRoot(root)
 
-const ctx = await Ctx.init()
+  const ctx = await Ctx.init()
 
-// @ts-ignore
-window.ctx = ctx
+  // @ts-ignore
+  window.ctx = ctx
 
-reactRoot.render(
-  <React.StrictMode>
-    <Ctx.Provider ctx={ctx}>
-      <App />
-    </Ctx.Provider>
-  </React.StrictMode>
-)
+  reactRoot.render(
+    <React.StrictMode>
+      <Ctx.Provider ctx={ctx}>
+        <App />
+      </Ctx.Provider>
+    </React.StrictMode>
+  )
+}
+
+main()
