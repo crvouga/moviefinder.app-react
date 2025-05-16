fd4f41ae9feb657aa7ba5b9088122bac14b760f3
crvouga <crvouga@gmail.com>
2025-05-16 15:36:56 -0700

    Enhance MediaPosterSwiper: integrate WrapIntersectionObserver for lazy loading of media items, improving performance by querying the media database only when items become visible. This update optimizes user interaction and resource management within the swiper component.

--- src/app/media/frontend/media-poster-swiper.tsx
+++ src/app/media/frontend/media-poster-swiper.tsx
@@ -1,11 +1,14 @@
+import { Clickable } from '~/@/ui/clickable'
+import { WrapIntersectionObserver } from '~/@/ui/intersection-observer'
 import { Swiper } from '~/@/ui/swiper'
+import { useCurrentScreen } from '~/app/@/screen/use-current-screen'
+import { useCtx } from '~/app/frontend/ctx'
 import { Media } from '../media'
 import { MediaPoster } from './media-poster'
-import { useCurrentScreen } from '~/app/@/screen/use-current-screen'
-import { Clickable } from '~/@/ui/clickable'
 
 export const MediaPosterSwiper = (props: { media: Media[] }) => {
   const currentScreen = useCurrentScreen()
+  const ctx = useCtx()
   return (
     <Swiper.Container slidesPerView="auto" spaceBetween={10} className="w-full">
       {props.media.map((m) => (
@@ -16,7 +19,17 @@ export const MediaPosterSwiper = (props: { media: Media[] }) => {
               currentScreen.push({ t: 'media-details', mediaId: m.id })
             }}
           >
-            <MediaPoster media={m} />
+            <WrapIntersectionObserver
+              onVisible={() => {
+                ctx.mediaDb.query({
+                  where: { op: '=', column: 'id', value: m.id },
+                  limit: 1,
+                  offset: 0,
+                })
+              }}
+            >
+              <MediaPoster media={m} />
+            </WrapIntersectionObserver>
           </Clickable>
         </Swiper.Slide>
       ))}
