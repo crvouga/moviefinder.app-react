9adc2aa24217c54621d05be05e31b0f7df20e22a
crvouga <crvouga@gmail.com>
2025-05-25 19:27:04 -0700

    Refactor AppVideoPlayer component for improved readability and maintainability: Simplified the conditional rendering logic by replacing the switch statement with an if statement. Updated button layout for better alignment and removed the minimize button functionality for clarity.

--- src/app/@/ui/app-video-player.tsx
+++ src/app/@/ui/app-video-player.tsx
@@ -18,35 +18,34 @@ const store = Store<State>({ t: 'closed' })
 export const AppVideoPlayer = () => {
   const state = useSyncExternalStore(store.subscribe, store.getSnapshot)
 
-  switch (state.t) {
-    case 'closed': {
-      return null
-    }
-    case 'opened': {
-      return (
-        <div className="absolute top-0 right-0 left-0 z-30">
-          <div className="overflow-bidden aspect-video w-full bg-black">
-            {state.video.key ? <YoutubeVideoPlayer youtubeVideoKey={state.video.key} /> : null}
-          </div>
+  if (state.t === 'closed') return null
 
-          <div className="flex flex-row gap-2">
-            <Button
-              text="Minimize"
-              color="primary"
-              variant="contained"
-              onClick={() => store.update(() => ({ t: 'closed' }))}
-            />
-            <Button
-              text="Close"
-              color="primary"
-              variant="contained"
-              onClick={() => store.update(() => ({ t: 'closed' }))}
-            />
-          </div>
-        </div>
-      )
-    }
-  }
+  return (
+    <div className="absolute top-0 right-0 left-0 z-30">
+      <div className="overflow-bidden aspect-video w-full bg-black">
+        {state.video.key ? <YoutubeVideoPlayer youtubeVideoKey={state.video.key} /> : null}
+      </div>
+
+      <div className="flex flex-row justify-end gap-2 px-2 py-1">
+        {false && (
+          <Button
+            text="Minimize"
+            color="primary"
+            variant="contained"
+            size="sm"
+            onClick={() => store.update(() => ({ t: 'closed' }))}
+          />
+        )}
+        <Button
+          text="Close"
+          color="primary"
+          variant="contained"
+          size="sm"
+          onClick={() => store.update(() => ({ t: 'closed' }))}
+        />
+      </div>
+    </div>
+  )
 }
 
 export const useAppVideoPlayer = () => {
